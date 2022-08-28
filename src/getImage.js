287553ecd5854e3cb29104459fa583c2ad32e5b7
Mohit Joshi <withmohitjoshi@gmail.com>
2022-08-28 20:01:06 +0530

    testing for long images

--- src/getImage.js
+++ src/getImage.js
@@ -3,8 +3,11 @@ async function getImage() {
   const randomNumber1 = Math.floor(Math.random() * 500);
   const randomNumber2 = Math.floor(Math.random() * 500);
 
+  // const res = await axios.get(
+  //   `https://picsum.photos/id/${randomNumber1 + randomNumber2 + 1}/info`
+  // );
   const res = await axios.get(
-    `https://picsum.photos/id/${randomNumber1 + randomNumber2 + 1}/info`
+    `https://picsum.photos/id/738/info`
   );
 
   return res;

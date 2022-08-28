181d0e65bdf98a52b15aca26552d3ba331c8c089
Mohit Joshi <withmohitjoshi@gmail.com>
2022-08-28 16:04:57 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./App.css";
 import Header from "./Components/Header";
 import BottomNav from "./Components/BottomNav";
 import ImageContainer from "./Components/ImageContainer";
